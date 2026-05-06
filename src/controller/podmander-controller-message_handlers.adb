f0d726c813a0858542386f948b80dc6a7c0bb205
Jochen Lillich <hello@monospacementor.com>
2026-05-06 22:43:32 +0100

    Upgraded heartbeat logging from Debug to Info
    Heartbeats were invisible at the default Info log level, making
    the controller appear hung after the initial status exchange.
    
    Issue: #15
    Tracker: forgejo
    Milestone: patch

--- src/controller/podmander-controller-message_handlers.adb
+++ src/controller/podmander-controller-message_handlers.adb
@@ -99,7 +99,7 @@ package body Podmander.Controller.Message_Handlers is
                Info.State := Podmander.Types.Registered;
             end if;
             H.Ctrl.Agents.Replace (Agent_Id, Info);
-            Podmander.Logging.Debug
+            Podmander.Logging.Info
               ("controller", "Heartbeat from " & Agent_Id);
          end;
       else
